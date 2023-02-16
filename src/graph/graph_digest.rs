6508b7f7c9082948114fde477abc22253d877158
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2023-02-16 16:55:52 +0100

    Add GraphDigtest

---
+++ src/graph/graph_digest.rs
@@ -0,0 +1,63 @@
+use super::*;
+use ::digest::Digest;
+
+pub trait GraphDigest {
+    /// Computes a Hash-Digest of a graph that is independent of the
+    /// graph data structure used and returns it as a hex string.
+    fn digest<D: Digest>(&self) -> String
+    where
+        digest::Output<D>: core::fmt::LowerHex;
+
+    /// Computes a SHA256 digest using [GraphDigest::digest]. The
+    /// returned string is 64 characters long.
+    fn digest_sha256(&self) -> String {
+        self.digest::<sha2::Sha256>()
+    }
+}
+
+impl<G: ColoredAdjacencyList> GraphDigest for G {
+    fn digest<D: Digest>(&self) -> String
+    where
+        digest::Output<D>: core::fmt::LowerHex,
+    {
+        let mut hasher = D::new();
+        let mut buffer = [0u8; 12];
+
+        let encode = |buf: &mut [u8], u: Node| {
+            for (i, c) in buf.iter_mut().enumerate().take(4) {
+                *c = (u >> (8 * i)) as u8;
+            }
+        };
+
+        // first encode the number of nodes in the graph
+        encode(&mut buffer[0..4], self.number_of_nodes());
+        hasher.update(buffer);
+
+        // then append a sorted edge list
+        for ColoredEdge(u, v, col) in self.ordered_colored_edges(true) {
+            encode(&mut buffer[0..], u);
+            encode(&mut buffer[4..], v);
+            encode(&mut buffer[8..], col as u32);
+            hasher.update(buffer);
+        }
+
+        format!("{:x}", hasher.finalize())
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use crate::graph::{AdjArray, EdgeColor, GraphDigest, GraphEdgeEditing, GraphNew};
+
+    #[test]
+    fn digest_sha256() {
+        let mut graph = AdjArray::new(10);
+        graph.add_edge(4, 3, EdgeColor::Black);
+        graph.add_edge(1, 2, EdgeColor::Red);
+        // computed with https://www.gnu.org/software/coreutils/sha256sum
+        assert_eq!(
+            graph.digest_sha256(),
+            "e941a4197b7ed2a797d7812b0e07e1e575f5f9f0c2963fbef365fc910fbcaf92"
+        );
+    }
+}
