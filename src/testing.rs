1db194f28d70c4c681a7b7a68b1ecf591d5ba110
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2023-03-29 18:50:37 +0200

    Allow reading small-random instances in parallel

--- src/testing.rs
+++ src/testing.rs
@@ -1,22 +1,41 @@
 use super::{graph::*, io::*};
+use rayon::prelude::*;
 use regex::Regex;
 
+#[allow(dead_code)]
 pub fn get_test_graphs_with_tww(pattern: &str) -> impl Iterator<Item = (String, AdjArray, Node)> {
     let files = glob::glob(pattern).expect("Invalid pattern");
     let regex_pattern = Regex::new(r"_tww(\d+)_").expect("Invalid regexp");
 
-    files.filter_map(move |filename| {
-        let filename = filename.ok()?;
-        let filename_string = String::from(filename.as_os_str().to_str()?);
+    files.filter_map(move |filename| process_file(filename, &regex_pattern))
+}
 
-        let tww = regex_pattern
-            .captures_iter(filename_string.as_str())
-            .next()?[1]
-            .parse()
-            .ok()?;
+#[allow(dead_code)]
+pub fn get_test_graphs_with_tww_in_par(
+    pattern: &str,
+) -> impl ParallelIterator<Item = (String, AdjArray, Node)> {
+    let files: Vec<_> = glob::glob(pattern).expect("Invalid pattern").collect();
+    let regex_pattern = Regex::new(r"_tww(\d+)_").expect("Invalid regexp");
 
-        let graph = AdjArray::try_read_pace_file(filename).ok()?;
+    files
+        .into_par_iter()
+        .filter_map(move |filename| process_file(filename, &regex_pattern))
+}
 
-        Some((filename_string, graph, tww))
-    })
+fn process_file(
+    filename: std::result::Result<std::path::PathBuf, glob::GlobError>,
+    regex_pattern: &Regex,
+) -> Option<(String, AdjArray, u32)> {
+    let filename = filename.ok()?;
+    let filename_string = String::from(filename.as_os_str().to_str()?);
+
+    let tww = regex_pattern
+        .captures_iter(filename_string.as_str())
+        .next()?[1]
+        .parse()
+        .ok()?;
+
+    let graph = AdjArray::try_read_pace_file(filename).ok()?;
+
+    Some((filename_string, graph, tww))
 }
