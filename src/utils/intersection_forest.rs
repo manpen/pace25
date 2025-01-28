manpen/pace25

commit 24745904d5ac845a1f4a06979f4cd9868037e0ba

make second case branchless using multiplication

src/utils/intersection_forest.rs
@@ -65,7 +65,7 @@ impl IntersectionTree {
                 let item = dest[ptr1];
                 let result = other[ptrr..].binary_search_by(|x| x.cmp(&item));
                 dest[ptrw] = item;
-                ptrw += result.is_ok() as usize;
+                ptrw += 1;
                 ptrr += result.unwrap_or_else(|x| x);
 
                 if ptrr == other.len() {
@@ -75,10 +75,9 @@ impl IntersectionTree {
         } else {
             for item in other {
                 let result = dest[ptrr..].binary_search_by(|x| x.cmp(item));
-                if result.is_ok() {
-                    dest[ptrw] = *item;
-                    ptrw += 1;
-                }
+                let is_ok = result.is_ok() as Node;
+                dest[ptrw] = is_ok * *item + (1 - is_ok) * dest[ptrw];
+                ptrw += is_ok as usize;
                 ptrr += result.unwrap_or_else(|x| x);
 
                 if ptrr == dest.len() {

commit d7a547fda6c8791a9a64a05a0bc67a82d175b105

fix bug introduced by typo in 24745904d5ac845a1f4a06979f4cd9868037e0ba

src/utils/intersection_forest.rs
@@ -65,7 +65,7 @@ impl IntersectionTree {
                 let item = dest[ptr1];
                 let result = other[ptrr..].binary_search_by(|x| x.cmp(&item));
                 dest[ptrw] = item;
-                ptrw += 1;
+                ptrw += result.is_ok() as usize;
                 ptrr += result.unwrap_or_else(|x| x);
 
                 if ptrr == other.len() {