6ca5a688aef7a89953b758f6b7f3997e44a15fb9
lukasgeis <lukasgeis@gmx.de>
2025-02-05 15:16:54 +0100

    remove round limit from testing

--- src/bin/heuristic_solver_bench.rs
+++ src/bin/heuristic_solver_bench.rs
@@ -32,7 +32,7 @@ fn main() -> anyhow::Result<()> {
     timer = Instant::now();
 
     let mut counter = 1usize;
-    while !signal_handling::received_ctrl_c() && !search.is_completed() && counter < 1000000 {
+    while !signal_handling::received_ctrl_c() && !search.is_completed() {
         counter += 1;
         search.step();
     }
