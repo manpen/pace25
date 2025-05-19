b9f4f8f56b00b09e8c5666f9e85bf56277d8b0c3
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2025-05-19 20:19:53 +0200

    Migrate subset rule into reductions mod. Broken

--- src/bin/exact_solver.rs
+++ src/bin/exact_solver.rs
@@ -4,7 +4,9 @@ use dss::{
     exact::{naive::naive_solver, sat_solver::SolverBackend},
     log::build_pace_logger_for_level,
     prelude::*,
-    reduction::{LongPathReduction, Reducer, RuleOneReduction, RuleSmallExactReduction},
+    reduction::{
+        LongPathReduction, Reducer, RuleOneReduction, RuleSmallExactReduction, RuleSubsetReduction,
+    },
 };
 use structopt::StructOpt;
 
@@ -88,7 +90,7 @@ fn main() -> anyhow::Result<()> {
     reducer.apply_rule_exhaustively::<RuleOneReduction<_>>(&mut graph, &mut solution, &mut covered);
     reducer.apply_rule::<LongPathReduction<_>>(&mut graph, &mut solution, &mut covered);
     reducer.apply_rule::<RuleSmallExactReduction<_>>(&mut graph, &mut solution, &mut covered);
-    let redundant = graph.vertex_bitset_unset(); // TODO: Add SubsetRule
+    let redundant = RuleSubsetReduction::apply_rule(&mut graph, &covered, &mut solution);
 
     let mut solution = {
         let csr_graph = CsrGraph::from_edges(graph.number_of_nodes(), graph.edges(true));
