883b89dc3bca62a2d37341e4e8b067b09fdca9cd
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2025-05-21 09:30:55 +0200

    Disable parsing of cmd and more aggressive size reductions

--- build_optil.sh
+++ build_optil.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash 
 set -e 
-export RUSTFLAGS='-C target-feature=+crt-static -C target-cpu=haswell -C target-feature=+bmi2,+bmi1' 
-BUILD_CMD="cargo build --target=x86_64-unknown-linux-musl --profile optil -F silent" 
+export RUSTFLAGS='-C target-feature=+crt-static -C target-cpu=haswell -C target-feature=+bmi2,+bmi1 -Zlocation-detail=none -Zfmt-debug=none' 
+BUILD_CMD="cargo build --target=x86_64-unknown-linux-musl --profile optil -F optil" 
 
 $BUILD_CMD --bin heuristic
 
