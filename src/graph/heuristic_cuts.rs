03b442b631772355374460204a793bb2ab3ba277
Johannes Meintrup <jmeintrup@googlemail.com>
2025-05-27 13:02:33 +0200

    Update optimal min cc size

--- src/graph/heuristic_cuts.rs
+++ src/graph/heuristic_cuts.rs
@@ -10,7 +10,7 @@ pub trait AdjacencyList {
 pub struct GraphCutBuilder<'a, G: AdjacencyList + ArticluationPoint + Traversal> {
     graph: &'a G,
     enabled_cuts: Vec<CutType>,
-    min_cc_size: NumNodes, // Minimum connected component size to keep
+    min_cc_size: Option<NumNodes>, // Minimum connected component size to keep
 }
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
@@ -25,7 +25,7 @@ impl<'a, G: AdjacencyList + ArticluationPoint + Traversal> GraphCutBuilder<'a, G
         Self {
             graph,
             enabled_cuts: vec![CutType::OneCut, CutType::TwoCut, CutType::ThreeCut],
-            min_cc_size: 5,
+            min_cc_size: None,
         }
     }
 
@@ -49,7 +49,7 @@ impl<'a, G: AdjacencyList + ArticluationPoint + Traversal> GraphCutBuilder<'a, G
         self
     }
 
-    pub fn min_cc_size(mut self, size: NumNodes) -> Self {
+    pub fn min_cc_size(mut self, size: Option<NumNodes>) -> Self {
         self.min_cc_size = size;
         self
     }
@@ -70,24 +70,26 @@ impl<'a, G: AdjacencyList + ArticluationPoint + Traversal> GraphCutBuilder<'a, G
 
     fn filter_cuts(&self, candidates: Vec<Vec<Node>>) -> Vec<Vec<Node>> {
         let mut good_cuts: Vec<Vec<Node>> = vec![];
-        for candidate in candidates {
-            let partition = self
-                .graph
-                .partition_into_connected_components_exclude_nodes(true, candidate.clone());
-            let mut smallest = 0;
-            let mut second_smallest = 0;
-            if partition.number_of_classes() > 1 {
-                for i in 0..partition.number_of_classes() {
-                    let a = partition.number_in_class(i);
-                    if a < smallest {
-                        second_smallest = smallest;
-                        smallest = a;
-                    } else if a < second_smallest {
-                        second_smallest = a;
+        if let Some(min_cc_size) = self.min_cc_size {
+            for candidate in candidates {
+                let partition = self
+                    .graph
+                    .partition_into_connected_components_exclude_nodes(true, candidate.clone());
+                let mut smallest = 0;
+                let mut second_smallest = 0;
+                if partition.number_of_classes() > 1 {
+                    for i in 0..partition.number_of_classes() {
+                        let a = partition.number_in_class(i);
+                        if a < smallest {
+                            second_smallest = smallest;
+                            smallest = a;
+                        } else if a < second_smallest {
+                            second_smallest = a;
+                        }
                     }
-                }
-                if second_smallest >= self.min_cc_size {
-                    good_cuts.push(candidate);
+                    if second_smallest >= min_cc_size {
+                        good_cuts.push(candidate);
+                    }
                 }
             }
         }
