7c9e4315c135a5891e9d5da274d71c661d3b3695
Lukas Geis <lgeis@ae.cs.uni-frankfurt.de>
2025-05-30 12:10:11 +0200

    Fix Redundant-Fix-Singleton-Error

--- src/reduction/rule_one_reduction.rs
+++ src/reduction/rule_one_reduction.rs
@@ -237,7 +237,16 @@ impl<Graph: AdjacencyList + GraphEdgeEditing + 'static> ReductionRule<Graph>
         covered.update_cleared_bits(|u| {
             let is_singleton = graph.degree_of(u) == 0;
             if is_singleton {
-                assert!(!redundant.get_bit(u));
+                // If redundant[u] = 1, then u was dominated by another node v that was removed in
+                // this iteration along with every other neighbor of u because u was the only
+                // uncovered neighbor of those nodes.
+                //
+                // It would be equally optimal to put v into the dominating set instead, but at
+                // this point, it does not matter.
+                //
+                // We nonetheless mark u was not-redundant anymore to prevent further checks from
+                // flagging this as unintended behavior
+                redundant.clear_bit(u);
                 domset.fix_node(u);
             }
             is_singleton
