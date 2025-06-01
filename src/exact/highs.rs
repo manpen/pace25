3aea264379b7d69aa465f1cef21583a0c3258062
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2025-06-01 17:51:05 +0200

    Use smaller models for highs

--- src/exact/highs.rs
+++ src/exact/highs.rs
@@ -3,10 +3,12 @@ use std::time::Duration;
 
 use highs::{HighsModelStatus, Model, RowProblem};
 use itertools::Itertools;
+use log::info;
 
 use crate::{graph::*, utils::DominatingSet};
+const NOT_SET: Node = Node::MAX;
 
-pub fn highs_solver<G: Clone + AdjacencyList + GraphEdgeEditing>(
+pub fn highs_solver<G: Clone + AdjacencyTest + AdjacencyList + GraphEdgeEditing>(
     graph: &G,
     is_perm_covered: &BitSet,
     never_select: &BitSet,
@@ -16,23 +18,67 @@ pub fn highs_solver<G: Clone + AdjacencyList + GraphEdgeEditing>(
     // TODO: RowProblems seems to get converted to a ColProblem --- so encode it directly as such
     let mut pb = RowProblem::default();
 
-    let vars = graph
-        .vertices_range()
+    let mut skip_constraints_of = graph.vertex_bitset_unset();
+    let mut skip_terms = 0;
+    for u in never_select.iter_set_bits() {
+        if graph.degree_of(u) != 2 {
+            continue;
+        }
+
+        if let Some((a, b)) = graph
+            .neighbors_of(u)
+            .filter(|&v| !never_select.get_bit(v))
+            .collect_tuple()
+            && graph.has_edge(a, b)
+        {
+            for x in [a, b] {
+                if !skip_constraints_of.set_bit(x) {
+                    skip_terms += graph
+                        .closed_neighbors_of(x)
+                        .filter(|&v| !never_select.get_bit(v))
+                        .count();
+                }
+            }
+        }
+    }
+    info!(
+        "Skip constraints of {} nodes with {skip_terms} edges",
+        skip_constraints_of.cardinality()
+    );
+
+    let vars = (0..(graph.number_of_nodes() - never_select.cardinality()))
         .map(|_| pb.add_integer_column(1.0, 0..1))
         .collect_vec();
 
+    let mut old_to_new = vec![NOT_SET; graph.len()];
+    let mut new_to_old = Vec::with_capacity(vars.len());
+
+    for old in never_select.iter_cleared_bits() {
+        old_to_new[old as usize] = new_to_old.len() as Node;
+        new_to_old.push(old);
+    }
+
+    let mut num_terms = 0;
     for u in graph.vertices_range() {
-        if is_perm_covered.get_bit(u) {
+        if is_perm_covered.get_bit(u) || skip_constraints_of.get_bit(u) {
             continue;
         }
 
         let coverable_by = graph
             .closed_neighbors_of(u)
             .filter(|&v| !never_select.get_bit(v))
-            .map(|v| (vars[v as usize], 1.0));
+            .map(|v| {
+                num_terms += 1;
+                (vars[old_to_new[v as usize] as usize], 1.0)
+            });
 
         pb.add_row(1.., coverable_by);
     }
+    info!(
+        "Remaining constraints: {}, Remaining terms: {num_terms}, Remaining vars: {}",
+        pb.num_rows(),
+        pb.num_cols()
+    );
 
     let mut model = Model::new(pb);
     model.make_quiet();
@@ -40,7 +86,6 @@ pub fn highs_solver<G: Clone + AdjacencyList + GraphEdgeEditing>(
         model.set_option("time_limit", tme.as_secs_f64());
     }
     model.set_option("parallel", "off");
-    model.set_option("threads", 1);
     model.set_sense(highs::Sense::Minimise);
 
     let solved = model.solve();
@@ -61,7 +106,7 @@ pub fn highs_solver<G: Clone + AdjacencyList + GraphEdgeEditing>(
             .columns()
             .iter()
             .enumerate()
-            .filter_map(|(i, &v)| (v > 0.5).then_some(i as Node)),
+            .filter_map(|(i, &v)| (v > 0.5).then_some(new_to_old[i])),
     );
 
     if upper_bound_incl.is_some_and(|b| b < domset.len() as NumNodes) {
