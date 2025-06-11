2aad91ef7193a1395e7636399544889c187c1818
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2025-06-11 10:32:19 +0200

    Add functions for generic reduction rule tests

--- src/testing.rs
+++ src/testing.rs
@@ -1,43 +1,115 @@
-use super::{graph::*, io::*};
-use rayon::prelude::*;
-use regex::Regex;
+use super::{
+    exact::naive::naive_solver,
+    graph::*,
+    prelude::*,
+    reduction::{Reducer, ReductionRule},
+};
+use itertools::Itertools as _;
+use rand::Rng;
 
-#[allow(dead_code)]
-pub fn get_test_graphs_with_tww<G: GraphPaceReader>(
-    pattern: &str,
-) -> impl Iterator<Item = (String, G, Node)> {
-    let files = glob::glob(pattern).expect("Invalid pattern");
-    let regex_pattern = Regex::new(r"_tww(\d+)_").expect("Invalid regexp");
+pub fn generate_random_graph_stream(
+    rng: &mut impl Rng,
+    n: NumNodes,
+) -> impl Iterator<Item = (AdjArray, BitSet, BitSet)> {
+    (0..).filter_map(move |i| {
+        let graph = AdjArray::random_gnp(rng, n, 3. / n as f64);
 
-    files.filter_map(move |filename| process_file(filename, &regex_pattern))
+        let mut covered = graph.vertex_bitset_unset();
+        for _ in 0..i % 7 {
+            covered.set_bit(rng.gen_range(graph.vertices_range()));
+        }
+        let mut redundant = graph.vertex_bitset_unset();
+        for _ in 0..i % 5 {
+            redundant.set_bit(rng.gen_range(graph.vertices_range()));
+        }
+        redundant -= &covered;
+
+        {
+            // reject if infeasible
+            let mut tmp = DominatingSet::new(graph.number_of_nodes());
+            tmp.add_nodes(redundant.iter_cleared_bits());
+            if !tmp.is_valid_given_previous_cover(&graph, &covered) {
+                return None;
+            }
+        }
+
+        for u in graph.vertices() {
+            if graph.degree_of(u) == 0 {
+                covered.set_bit(u);
+            }
+        }
+
+        Some((graph, covered, redundant))
+    })
 }
 
-#[allow(dead_code)]
-pub fn get_test_graphs_with_tww_in_par<G: GraphPaceReader + Send + Sync>(
-    pattern: &str,
-) -> impl ParallelIterator<Item = (String, G, Node)> {
-    let files: Vec<_> = glob::glob(pattern).expect("Invalid pattern").collect();
-    let regex_pattern = Regex::new(r"_tww(\d+)_").expect("Invalid regexp");
+pub fn test_before_and_after_rule<C: FnMut(&AdjArray) -> R, R: ReductionRule<AdjArray>>(
+    rng: &mut impl Rng,
+    mut rule_cons: C,
+    nodes: NumNodes,
+    attempts: u32,
+) {
+    let mut num_applicable = 0;
+    let max_attempts = attempts * 50;
+    for (mut graph, mut covered, mut never_select) in
+        generate_random_graph_stream(rng, nodes).take(max_attempts as usize)
+    {
+        let org_graph = graph.clone();
+        let org_covered = covered.clone();
+        let org_never_select = never_select.clone();
+        let naive = naive_solver(&graph, &covered, &never_select, None, None).unwrap();
 
-    files
-        .into_par_iter()
-        .filter_map(move |filename| process_file(filename, &regex_pattern))
-}
+        let after_rule = {
+            let mut domset = DominatingSet::new(graph.number_of_nodes());
+            let mut reducer = Reducer::new();
+            reducer.remove_unnecessary_edges(
+                &mut graph,
+                &mut domset,
+                &mut covered,
+                &mut never_select,
+            );
+            let mut rule = rule_cons(&graph);
 
-fn process_file<G: GraphPaceReader>(
-    filename: std::result::Result<std::path::PathBuf, glob::GlobError>,
-    regex_pattern: &Regex,
-) -> Option<(String, G, u32)> {
-    let filename = filename.ok()?;
-    let filename_string = String::from(filename.as_os_str().to_str()?);
+            let changed = reducer.apply_rule(
+                &mut rule,
+                &mut graph,
+                &mut domset,
+                &mut covered,
+                &mut never_select,
+            );
+            num_applicable += changed as u32;
 
-    let tww = regex_pattern
-        .captures_iter(filename_string.as_str())
-        .next()?[1]
-        .parse()
-        .ok()?;
+            let tmp = naive_solver(&graph, &covered, &never_select, None, None).unwrap();
+            domset.add_nodes(tmp.iter());
+            domset
+        };
 
-    let graph = G::try_read_pace_file(filename).ok()?;
+        assert!(after_rule.is_valid_given_previous_cover(&graph, &covered));
+        assert_eq!(
+            naive.len(),
+            after_rule.len(),
+            "naive: {:?}, after_rule: {:?}, org: {org_graph:?}, cov: {:?}, red: {:?}",
+            naive.iter().collect_vec(),
+            after_rule.iter().collect_vec(),
+            org_covered.iter_set_bits().collect_vec(),
+            org_never_select.iter_set_bits().collect_vec()
+        );
+        assert!(
+            after_rule.iter().all(|u| !never_select.get_bit(u)),
+            "naive: {:?}, after_rule: {:?}, org: {org_graph:?}, cov: {:?}, red: {:?}",
+            naive.iter().collect_vec(),
+            after_rule.iter().collect_vec(),
+            org_covered.iter_set_bits().collect_vec(),
+            org_never_select.iter_set_bits().collect_vec()
+        );
 
-    Some((filename_string, graph, tww))
+        if num_applicable > attempts {
+            break;
+        }
+    }
+
+    assert!(
+        num_applicable >= attempts,
+        "Abort test as rule was only applicable {num_applicable} times within {max_attempts} attempts"
+    );
 }
